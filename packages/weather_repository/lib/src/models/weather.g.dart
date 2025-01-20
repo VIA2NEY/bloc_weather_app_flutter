16e8b72c8e554490ad9726dec36b5b23ac764f5b
VIA2NEY <kouameangeV11@gmail.com>
2025-01-20 15:32:48 +0000

    finish setup of packages\weather_repository

---
+++ packages/weather_repository/lib/src/models/weather.g.dart
@@ -0,0 +1,67 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Weather _$WeatherFromJson(Map<String, dynamic> json) => $checkedCreate(
+      'Weather',
+      json,
+      ($checkedConvert) {
+        final val = Weather(
+          location: $checkedConvert('location', (v) => v as String),
+          temperature:
+              $checkedConvert('temperature', (v) => (v as num).toDouble()),
+          condition: $checkedConvert(
+              'condition', (v) => $enumDecode(_$WeatherConditionEnumMap, v)),
+        );
+        return val;
+      },
+    );
+
+Map<String, dynamic> _$WeatherToJson(Weather instance) => <String, dynamic>{
+      'location': instance.location,
+      'temperature': instance.temperature,
+      'condition': _$WeatherConditionEnumMap[instance.condition]!,
+    };
+
+const _$WeatherConditionEnumMap = {
+  WeatherCondition.clear: 'clear',
+  WeatherCondition.rainy: 'rainy',
+  WeatherCondition.cloudy: 'cloudy',
+  WeatherCondition.snowy: 'snowy',
+  WeatherCondition.unknown: 'unknown',
+};
+
+
+// Sans Ajouter le build.yaml
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// part of 'weather.dart';
+
+// // **************************************************************************
+// // JsonSerializableGenerator
+// // **************************************************************************
+
+// Weather _$WeatherFromJson(Map<String, dynamic> json) => Weather(
+//       location: json['location'] as String,
+//       temperature: (json['temperature'] as num).toDouble(),
+//       condition: $enumDecode(_$WeatherConditionEnumMap, json['condition']),
+//     );
+
+// Map<String, dynamic> _$WeatherToJson(Weather instance) => <String, dynamic>{
+//       'location': instance.location,
+//       'temperature': instance.temperature,
+//       'condition': _$WeatherConditionEnumMap[instance.condition]!,
+//     };
+
+// const _$WeatherConditionEnumMap = {
+//   WeatherCondition.clear: 'clear',
+//   WeatherCondition.rainy: 'rainy',
+//   WeatherCondition.cloudy: 'cloudy',
+//   WeatherCondition.snowy: 'snowy',
+//   WeatherCondition.unknown: 'unknown',
+// };
