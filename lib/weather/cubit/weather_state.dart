50649112d3540a1af190b575005e1bf4b0b88ca3
VIA2NEY <kouameangeV11@gmail.com>
2025-01-21 10:20:47 +0000

    Presentation layer over and the app is working

---
+++ lib/weather/cubit/weather_state.dart
@@ -0,0 +1,55 @@
+part of 'weather_cubit.dart';
+
+// sealed class WeatherState extends Equatable {
+//   const WeatherState();
+
+//   @override
+//   List<Object> get props => [];
+// }
+
+// final class WeatherInitial extends WeatherState {}
+
+
+
+
+enum WeatherStatus { initial, loading, success, failure }
+
+extension WeatherStatusX on WeatherStatus {
+  bool get isInitial => this == WeatherStatus.initial;
+  bool get isLoading => this == WeatherStatus.loading;
+  bool get isSuccess => this == WeatherStatus.success;
+  bool get isFailure => this == WeatherStatus.failure;
+}
+
+@JsonSerializable()
+final class WeatherState extends Equatable {
+  WeatherState({
+    this.status = WeatherStatus.initial,
+    this.temperatureUnits = TemperatureUnits.celsius,
+    Weather? weather,
+  }) : weather = weather ?? Weather.empty;
+
+  factory WeatherState.fromJson(Map<String, dynamic> json) =>
+      _$WeatherStateFromJson(json);
+
+  final WeatherStatus status;
+  final Weather weather;
+  final TemperatureUnits temperatureUnits;
+
+  WeatherState copyWith({
+    WeatherStatus? status,
+    TemperatureUnits? temperatureUnits,
+    Weather? weather,
+  }) {
+    return WeatherState(
+      status: status ?? this.status,
+      temperatureUnits: temperatureUnits ?? this.temperatureUnits,
+      weather: weather ?? this.weather,
+    );
+  }
+
+  Map<String, dynamic> toJson() => _$WeatherStateToJson(this);
+
+  @override
+  List<Object?> get props => [status, temperatureUnits, weather];
+}
